9441eecd8f72179e70020440fdc9af42a4556c21
srushtixvasani <srushtivasani@gmail.com>
2021-03-16 22:01:03 +0000

    Still need to fix font color and font. Add text in breaths taken and minutes. Animation started working need to tweak it tho.
    Signed-off-by: srushtixvasani <srushtivasani@gmail.com>

--- app/src/main/java/com/example/dailynotdilly/BreatheActivity.java
+++ app/src/main/java/com/example/dailynotdilly/BreatheActivity.java
@@ -3,19 +3,24 @@ package com.example.dailynotdilly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
+import com.github.florent37.viewanimator.ViewAnimator;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BreatheActivity extends AppCompatActivity {
 
+    private ImageView breatheImage;
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.breathe_activity);
-
 
         // set floating action button to open home screen
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -27,5 +32,42 @@ public class BreatheActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        breatheImage = findViewById(R.id.breatheImage);
+
+        ViewAnimator
+                .animate(breatheImage)
+                .translationY(-1000, 0)
+                .alpha(0,1)
+                .dp().translationX(-20, 0)
+                .decelerate()
+                .duration(2000)
+                .thenAnimate(breatheImage)
+                .scale(1f, 0.5f, 1f)
+                .rotation(360f)
+                .repeatCount(10)
+                .accelerate()
+                .duration(2000)
+                .start();
+
+       }
+
+//    private View.OnClickListener startViewAnimation() {
+//        ImageView breatheImage = findViewById(R.id.breatheImage);
+//        ViewAnimator viewAnimator = new ViewAnimator();
+//
+//        viewAnimator.animate(breatheImage)
+//                .rotation(360)
+//                .start();
+////                .scale(0.2f,1.5f,0.02f)
+////                .rotation(360f)
+////                .repeatCount(10)
+////                .accelerate()
+////                .duration(20000)
+////                .onStop(() -> {
+////                    breatheImage.setScaleX(1.0f);
+////                    breatheImage.setScaleY(1.0f);
+////                })
+////                .onStart();
+//    }
 }
