92ca0ca8d1dc9a941c1e7515d5f9f40f4a4c395b
srushtixvasani <srushtivasani@gmail.com>
2021-03-24 21:19:33 +0000

    Breathe Feature finished
    Signed-off-by: srushtixvasani <srushtivasani@gmail.com>

--- app/src/main/java/com/example/dailynotdilly/BreatheActivity.java
+++ app/src/main/java/com/example/dailynotdilly/BreatheActivity.java
@@ -2,28 +2,38 @@ package com.example.dailynotdilly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
 
+import com.example.dailynotdilly.utils.Preferences;
 import com.github.florent37.viewanimator.AnimationListener;
 import com.github.florent37.viewanimator.ViewAnimator;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.MessageFormat;
+
 public class BreatheActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
     private ImageView breatheImage;
-    private AppCompatTextView minute;
+    private AppCompatTextView lastSession;
     private AppCompatTextView numOfBreathTaken;
-    private AppCompatTextView minsToday;
+    private AppCompatTextView session;
     private MaterialButton startButton;
-    private MaterialButton stopButton;
+    private Preferences preferences;
+    private ImageButton infoButton;
+
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
@@ -41,36 +51,36 @@ public class BreatheActivity extends AppCompatActivity {
             }
         });
 
+        // information for the user on how to use the Breathe feature
+        infoButton = findViewById(R.id.info_button);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfoWindow(v);
+            }
+        });
+
         // Statistic for the user
-        minute = findViewById(R.id.minute);
+        preferences = new Preferences(this);
+        lastSession = findViewById(R.id.lastSession);
         numOfBreathTaken = findViewById(R.id.numOfBreathTaken);
-        minsToday = findViewById(R.id.minsToday);
+        session = findViewById(R.id.minsToday);
 
-
-
+        session.setText(MessageFormat.format("{0} minutes total", preferences.getSessions()));
+        numOfBreathTaken.setText(MessageFormat.format("{0} breaths taken", preferences.getBreathsTaken()));
+        lastSession.setText(preferences.getDate());
 
         // for the animation to go smaller when user inhales, larger when user exhales when start button is pressed
         breatheImage = findViewById(R.id.breatheImage);
 
+        // to start animation
         startButton = findViewById(R.id.breatheButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startAnimation();
             }
-
         });
-
-        // to stop animation
-        stopButton = findViewById(R.id.stopBreathButton);
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopAnimation();
-            }
-        });
-
-
        }
 
        private void startAnimation(){
@@ -79,32 +89,59 @@ public class BreatheActivity extends AppCompatActivity {
                    .scale(1f, 0.75f, 0.5f, 0.75f, 1f)
                    .decelerate()
                    .duration(7000)
-                   .repeatCount(ViewAnimator.INFINITE)
+                   .repeatCount(85) //should be 85 for the session to be 10 minutes
+                   .onStop(new AnimationListener.Stop() {
+                       @Override
+                       public void onStop() {
+                           breatheImage.setScaleX(1f);
+                           breatheImage.setScaleX(1f);
+
+                           preferences.setDate(System.currentTimeMillis());
+                           preferences.setSessions(preferences.getSessions() + 10);
+                           preferences.setBreathsTaken(preferences.getBreathsTaken() + 86);
+
+                           // when duration is completed, the activity should be refreshed
+                           new CountDownTimer(2500, 1000){
+
+                               @Override
+                               public void onTick(long millisUntilFinished) {
+                                   // code to show ticking, but it is not needed in this case
+                               }
+
+                               @Override
+                               public void onFinish() {
+                                    startActivity(new Intent(getApplicationContext(), BreatheActivity.class));
+                                    finish();
+                               }
+                           }.start();
+                       }
+                   })
                    .start();
        }
 
-       private void stopAnimation(){
-            ViewAnimator.animate(breatheImage)
-                    .start().cancel();
-       }
+    // info window using PopupWindow to be used with the info_button.
+    public void showInfoWindow(View view) {
 
+        // inflate the layout of the info window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View infoWindow = inflater.inflate(R.layout.info_window, null);
 
-//    private View.OnClickListener startViewAnimation() {
-//        ImageView breatheImage = findViewById(R.id.breatheImage);
-//        ViewAnimator viewAnimator = new ViewAnimator();
-//
-//        viewAnimator.animate(breatheImage)
-//                .rotation(360)
-//                .start();
-////                .scale(0.2f,1.5f,0.02f)
-////                .rotation(360f)
-////                .repeatCount(10)
-////                .accelerate()
-////                .duration(20000)
-////                .onStop(() -> {
-////                    breatheImage.setScaleX(1.0f);
-////                    breatheImage.setScaleY(1.0f);
-////                })
-////                .onStart();
-//    }
+        // create the info window
+        int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+        final PopupWindow infoPopup = new PopupWindow(infoWindow, width, height, true);
+
+        // shows the info window
+        infoPopup.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismisses the info window when clicked
+        infoWindow.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                infoPopup.dismiss();
+                return true;
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/dailynotdilly/utils/Preferences.java
@@ -0,0 +1,60 @@
+package com.example.dailynotdilly.utils;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+import java.util.Calendar;
+
+public class Preferences {
+
+    private SharedPreferences preferences;
+
+    // create shared preferences which will be saved in the device
+    public Preferences(Activity activity){
+        this.preferences = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    public void setBreathsTaken(int numOfBreaths){
+        // saves the number of breaths taken
+        preferences.edit().putInt("numOfBreaths" , numOfBreaths).apply();
+    }
+
+    public void setSessions(int session){
+        // saves the number of sessions taken
+        preferences.edit().putInt("sessions" , session).apply();
+    }
+
+    public void setDate(long milliSecs){
+        preferences.edit().putLong("seconds", milliSecs).apply();
+    }
+
+    public int getBreathsTaken(){
+        return preferences.getInt("numOfBreaths", 0);
+    }
+
+    public int getSessions(){
+        return preferences.getInt("sessions", 0);
+    }
+
+    public String getDate(){
+        long dateInMilli = preferences.getLong("seconds", 0);
+        String amPm; // if the date is in am or pm
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(dateInMilli);
+
+        int x = calendar.get(Calendar.AM_PM);
+        if (x == Calendar.AM)
+            amPm = "AM";
+        else
+            amPm = "PM";
+
+        String time = "Last session at " + calendar.get(Calendar.HOUR_OF_DAY) + ":" +
+                calendar.get(Calendar.MINUTE) + " " + amPm;
+        return time;
+    }
+
+
+
+
+}
