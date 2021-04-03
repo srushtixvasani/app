9516214da41343f6397cf73d2f88932332bf4630
srushtixvasani <srushtivasani@gmail.com>
2021-04-04 00:03:57 +0100

    Started to-do feature
    Signed-off-by: srushtixvasani <srushtivasani@gmail.com>

---
+++ app/src/main/java/com/example/dailynotdilly/ToDoListBottomFragment.java
@@ -0,0 +1,29 @@
+package com.example.dailynotdilly;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class ToDoListBottomFragment extends BottomSheetDialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.to_do_list_bottom_sheet, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
